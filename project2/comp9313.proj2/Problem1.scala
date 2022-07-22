930192bf155c3a4c59472d99b8995f851ebd30b7
abdullah <abdullah527382@gmail.com>
2022-07-23 00:55:31 +1000

    Put the code into a function

--- project2/comp9313.proj2/Problem1.scala
+++ project2/comp9313.proj2/Problem1.scala
@@ -4,6 +4,8 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.rdd.RDD;
+import org.apache.spark.broadcast.Broadcast
+
 object Problem1 {
   def main(args: Array[String]): Unit = {
 
@@ -24,58 +26,39 @@ object Problem1 {
 
     var filteredData: Array[String] = Array[String]()
     stopWordsBroadcast.value.foreach(word => {
-      if (filteredData.isEmpty) {
-        filteredData = sampleBroadcast.value.map(line => {
-          var lineArray = line.split("[, ]").filter(_ != word);
-          var concatString: String = lineArray(0) + ","
-          for (i <- 1 to lineArray.length - 1) {
-            val lineWord = lineArray(i)
-            concatString += " " + lineWord
-          }
-          concatString
-        })
-      } else {
-        filteredData = filteredData.map(line => {
-          var lineArray = line.split(" ").filter(_ != word);
-          var concatString: String = lineArray(0)
-          for (i <- 1 to lineArray.length - 1) {
-            val lineWord = lineArray(i)
-            concatString += " " + lineWord
-          }
-          concatString.replace(", ", ",")
-        })
-      }
+      filteredData = filterStopWords(sampleBroadcast, filteredData, word)
     })
 
     filteredData.foreach(word => {
       println(word)
     })
-    // // val filteredData = sampleData.map(line => line.replace("to", "REMOVED"))
-    // filteredData.foreach(line => println(line))
-    // sampleData.flatMap(_.split("[, ]")).foreach(line => println(line))
-
   }
 
-  // Given an array of strings, remove a stopword
-  def removeStopWord(
-      words: String,
-      stopWords: RDD[String]
-  ): ArrayBuffer[String] = {
-    val cleanLine = new ArrayBuffer[String]
-
-    // Break the line into an array of words:
-    val wordArray = words.split(" ")
-    // Loop through the stop words
-    stopWords.foreach(stopWord => {
-      for (i <- 0 to wordArray.length - 1) {
-        val lineWord = wordArray(i)
-        if (lineWord != stopWord) {
-          cleanLine.+=:(lineWord)
+  def filterStopWords(
+      sampleBroadcast: Broadcast[Array[String]],
+      filteredData: Array[String],
+      word: String
+  ): Array[String] = {
+    if (filteredData.isEmpty) {
+      return sampleBroadcast.value.map(line => {
+        var lineArray = line.split("[, ]").filter(_ != word);
+        var concatString: String = lineArray(0) + ","
+        for (i <- 1 to lineArray.length - 1) {
+          val lineWord = lineArray(i)
+          concatString += " " + lineWord
         }
-      }
-    })
-    println(cleanLine)
-    return cleanLine
+        concatString
+      })
+    } else {
+      return filteredData.map(line => {
+        var lineArray = line.split(" ").filter(_ != word);
+        var concatString: String = lineArray(0)
+        for (i <- 1 to lineArray.length - 1) {
+          val lineWord = lineArray(i)
+          concatString += " " + lineWord
+        }
+        concatString.replace(", ", ",")
+      })
+    }
   }
-
 }
