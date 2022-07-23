b2b88d8eec5d87afdd09cc7285c20b223326ad91
abdullah <abdullah527382@gmail.com>
2022-07-23 21:56:04 +1000

    Need to sort the output alphabetically by List[0], need to get args and write to file

--- project2/comp9313.proj2/Problem1.scala
+++ project2/comp9313.proj2/Problem1.scala
@@ -5,14 +5,16 @@ import org.apache.spark.SparkConf
 import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.rdd.RDD;
 import org.apache.spark.broadcast.Broadcast
+import scala.collection.immutable.ListMap
 
 object Problem1 {
   def main(args: Array[String]): Unit = {
 
-    // Some file in the hdfs fs
+    // Some file in the hdfs fs, replace these as arg variables
     val sampleFileAddr = "project2/tiny-doc.txt"
     val stopWordsFileAddr = "project2/stopwords.txt"
     val outputFolder = "project2/output"
+    val K = 3
     // Create a new spark configuration and create a context out of that
     val conf = new SparkConf().setAppName("Problem1").setMaster("local")
     val sc = new SparkContext(conf)
@@ -29,9 +31,22 @@ object Problem1 {
       filteredData = filterStopWords(sampleBroadcast, filteredData, word)
     })
 
-    filteredData.foreach(word => {
-      println(word)
-    })
+    filteredData = removeDate(filteredData)
+    val tokens = filteredData.map(_.split(" ").toList)
+
+    // Tokens is a List of strings: List[String]
+    val coo = tokens
+      .flatMap(_.combinations(2))
+      .map((_, 1))
+      .groupBy(_._1)
+      .map { case (key, list) => key -> list.map(_._2).reduce(_ + _) }
+
+    // Sort alphhabetically
+    val res = ListMap(coo.toSeq.sortBy(-_._2): _*).take(3)
+    // res.foreach(println)
+    res.foreach { case (key, value) =>
+      println(key(0) + "," + key(1) + "\t" + value)
+    }
   }
 
   def filterStopWords(
@@ -61,4 +76,17 @@ object Problem1 {
       })
     }
   }
+
+  // Remove the date from our Array[String] since it seems pointless
+  def removeDate(
+      filteredData: Array[String]
+  ): Array[String] = {
+    return filteredData.map(line => {
+      var lineArray = line.split(",");
+      lineArray(1)
+    })
+  }
+
+  //
+
 }
