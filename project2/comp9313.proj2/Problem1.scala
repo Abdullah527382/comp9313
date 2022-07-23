a45b333552712ea80027c7e55bfda4849ededc93
abdullah <abdullah527382@gmail.com>
2022-07-24 01:33:30 +1000

    Put the code into a function and now part 1 complete

--- project2/comp9313.proj2/Problem1.scala
+++ project2/comp9313.proj2/Problem1.scala
@@ -11,10 +11,10 @@ object Problem1 {
   def main(args: Array[String]): Unit = {
 
     // Some file in the hdfs fs, replace these as arg variables
-    val sampleFileAddr = "project2/tiny-doc.txt"
-    val stopWordsFileAddr = "project2/stopwords.txt"
-    val outputFolder = "project2/output"
-    val K = 3
+    val K = args(0) // e.g. 3
+    val stopWordsFileAddr = args(1) // e.g. "project2/stopwords.txt"
+    val sampleFileAddr = args(2) // e.g. "project2/tiny-doc.txt"
+    val outputFolder = args(3) // e.g. "project2/output"
     // Create a new spark configuration and create a context out of that
     val conf = new SparkConf().setAppName("Problem1").setMaster("local")
     val sc = new SparkContext(conf)
@@ -34,17 +34,23 @@ object Problem1 {
     filteredData = removeDate(filteredData)
     val tokens = filteredData.map(_.split(" ").toList)
 
-    // Tokens is a List of strings: List[String]
     val coo = tokens
       .flatMap(_.combinations(2))
       .map((_, 1))
       .groupBy(_._1)
       .map { case (key, list) => key -> list.map(_._2).reduce(_ + _) }
+    //   .toSeq
+    //   .sortWith((list: (List[String], Int)) => list._1(0) > list._1(1))
+    // // coo.foreach(println)
 
     // Sort alphhabetically
     val res = ListMap(coo.toSeq.sortBy(-_._2): _*).take(3)
+    // case class record(list: List[String], count: Int)
+    val sorted =
+      res.toSeq.sortBy(_._1(0))
+
     // res.foreach(println)
-    res.foreach { case (key, value) =>
+    sorted.foreach { case (key, value) =>
       println(key(0) + "," + key(1) + "\t" + value)
     }
   }
